deb402fe0986cfa934675d12b1f87b9af347f10f
Bartosz Sass <sassbartek@gmail.com>
2025-04-27 12:36:11 +0100

    Missing MFA imports.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { Register, Login } from "../controllers/auth.js";
 import { VerifyToken } from "../middleware/token.js";
+import { Setup2FA, Verify2FA } from "../controllers/mfa.js";
 
 const router = express.Router();
 
